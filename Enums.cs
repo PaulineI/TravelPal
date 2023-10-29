d883f2ae72d83af88a64ce03f402d88f92a6f3ce
PaulineI <pauline.ibraim@gmail.com>
2023-10-29 11:49:58 +0100

    Enums

---
+++ Enums.cs
@@ -0,0 +1,55 @@
+﻿namespace TravelPal
+{
+
+    enum Country
+    {
+        Albania,
+        Andorra,
+        Austria,
+        Belarus,
+        Belgium,
+        Bosnia,
+        Bulgaria,
+        Croatia,
+        Cyprus,
+        CzechRepublic,
+        Denmark,
+        Estonia,
+        Finland,
+        France,
+        Germany,
+        Greece,
+        Hungary,
+        Iceland,
+        Ireland,
+        Italy,
+        Kosovo,
+        Latvia,
+        Liechtenstein,
+        Lithuania,
+        Luxembourg,
+        Malta,
+        Moldova,
+        Monaco,
+        Montenegro,
+        Netherlands,
+        NorthMacedonia,
+        Norway,
+        Poland,
+        Portugal,
+        Romania,
+        Russia,
+        SanMarino,
+        Serbia,
+        Slovakia,
+        Slovenia,
+        Spain,
+        Sweden,
+        Switzerland,
+        Ukraine,
+        UnitedKingdom
+    }
+
+
+
+}
